a4d31437f93ba7f9b30d1248d952eabec794f907
Tom Pluse <plusetom@gmail.com>
2023-12-12 21:30:47 +0100

    fixed bugs. complete functionality.

--- src/util/spotify.js
+++ src/util/spotify.js
@@ -26,22 +26,24 @@ const Spotify = {
     const accessToken = Spotify.getAccessToken();
     return fetch(`https://api.spotify.com/v1/search?type=track&q=${term}`, {
       headers: {
-        Authorization: `Bearer ${accessToken}`
-      }
-    }).then(response => {
-      return response.json();
-    }).then(jsonResponse => {
-      if (!jsonResponse.tracks) {
-        return [];
-      }
-      return jsonResponse.tracks.items.map(track => ({
-        id: track.id,
-        name: track.name,
-        artist: track.artists[0].name,
-        album: track.album.name,
-        uri: track.uri
-      }));
-    });
+        Authorization: `Bearer ${accessToken}`,
+      },
+    })
+      .then((response) => {
+        return response.json();
+      })
+      .then((jsonResponse) => {
+        if (!jsonResponse.tracks) {
+          return [];
+        }
+        return jsonResponse.tracks.items.map((track) => ({
+          id: track.id,
+          name: track.name,
+          artist: track.artists[0].name,
+          album: track.album.name,
+          uri: track.uri,
+        }));
+      });
   },
 
   savePlaylist(name, trackUris) {
@@ -66,11 +68,11 @@ const Spotify = {
           .then((jsonResponse) => {
             const playlistId = jsonResponse.id;
             return fetch(
-              `https://api.spotify.com/v1/users/${userId}/playlists/${playlistId}`,
+              `https://api.spotify.com/v1/users/${userId}/playlists/${playlistId}/tracks`,
               {
                 headers: headers,
                 method: "POST",
-                uri: JSON.stringify({ uri: trackUris }),
+                body: JSON.stringify({ uris: trackUris }),
               }
             );
           });
